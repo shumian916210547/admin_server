405c3ead0732f943ad88c905dbd3c51b8e962344
shumian <916210547@qq.com>
2022-11-05 21:12:10 +0800

    修改host

--- databaseConfig.js
+++ databaseConfig.js
@@ -1,12 +1,12 @@
-let host = "localhost";
+let host = "114.215.210.204";
 
-if (process.env.NODE_ENV == "development") {
+/* if (process.env.NODE_ENV == "development") {
   host = "localhost";
 }
 
 if (process.env.NODE_ENV == "production") {
   host = "114.215.210.204";
-}
+} */
 
 module.exports = {
   user: "postgres",
