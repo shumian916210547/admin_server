0bc20ba950dbed3f3d5a17b994329eb0c0d214d6
shumian <916210547@qq.com>
2023-02-07 15:39:51 +0800

    修改配置

--- databaseConfig.js
+++ databaseConfig.js
@@ -1,13 +1,13 @@
-let host = "api.shumian.top";
+let host = "localhost";
 
-if (process.env.NODE_ENV == "development") {
+/* if (process.env.NODE_ENV == "development") {
   host = "localhost";
 }
 
 if (process.env.NODE_ENV == "production") {
   host = "api.shumian.top";
 }
-
+ */
 module.exports = {
   user: "postgres",
   database: "postgres",
