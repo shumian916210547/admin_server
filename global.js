8e957ac63f7375da6491ff474255a270ea1c7697
shumian <916210547@qq.com>
2023-03-01 23:04:58 +0800

    修复日期错误显示

--- global.js
+++ global.js
@@ -1,16 +1,16 @@
 const path = require("path");
 const Parse = require("parse/node");
-const multer = require('multer')
+const multer = require("multer");
 global._require = (filePath) => {
   return require(path.join(process.cwd(), filePath));
 };
-const date = new Date()
-const year = date.getFullYear()
+const date = new Date();
+const year = date.getFullYear();
 const month = date.getMonth();
 const day = date.getDate();
 Parse.initialize("shumian0511");
 Parse.masterKey = "shumian100329";
 Parse.serverURL = "http://localhost:3000/parse";
-global.Parse = Parse
-global.todayStatic = 'resources/' + year + '/' + month + '/' + day + '/'
-global.upload = multer({ dest: todayStatic })
+global.Parse = Parse;
+global.todayStatic = "resources/" + year + "/" + (month + 1) + "/" + day + "/";
+global.upload = multer({ dest: todayStatic });
