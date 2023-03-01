319bde64f79bd0ea48cfb30fe61735db49f9dd0e
shumian <916210547@qq.com>
2023-03-01 14:28:58 +0800

    优化parse cloud文件结构

---
+++ Cloud/Task.js
@@ -0,0 +1,56 @@
+const Task = {
+  beforeSave: [],
+  afterSave: [
+    {
+      class: 'Task',
+      func: async (request) => {
+        const task = request.object.toJSON();
+        const classid = task.class.objectId;
+        const taskid = task.objectId;
+        const companyid = task.company.objectId;
+        const Student = new Parse.Query("Student");
+        Student.equalTo("class", classid);
+        const students = (await Student.find()).map((item) => {
+          return item.toJSON();
+        });
+        for (const stu of students) {
+          const td = new Parse.Query("TaskDetail");
+          td.equalTo("task", taskid);
+          td.equalTo("student", stu.objectId);
+          const count = await td.count();
+          if (!count) {
+            const TaskDetail = Parse.Object.extend("TaskDetail");
+            const taskDetail = new TaskDetail();
+            taskDetail.set("status", false);
+            taskDetail.set("isDelete", false);
+            taskDetail.set("company", {
+              __type: "Pointer",
+              className: "Company",
+              objectId: companyid,
+            });
+            taskDetail.set("task", {
+              __type: "Pointer",
+              className: "Task",
+              objectId: taskid,
+            });
+            taskDetail.set("class", {
+              __type: "Pointer",
+              className: "Class",
+              objectId: classid,
+            });
+            taskDetail.set("student", {
+              __type: "Pointer",
+              className: "Student",
+              objectId: stu.objectId,
+            });
+            await taskDetail.save();
+          }
+        }
+        return
+      }
+    }
+  ],
+  beforeDelete: [],
+  afterDelete: []
+}
+module.exports = Task
--- cloud.js
+++ cloud.js
@@ -1,44 +1,13 @@
-Parse.Cloud.afterSave("Task", async function (request) {
-  const task = request.object.toJSON();
-  const classid = task.class.objectId;
-  const taskid = task.objectId;
-  const companyid = task.company.objectId;
-  const Student = new Parse.Query("Student");
-  Student.equalTo("class", classid);
-  const students = (await Student.find()).map((item) => {
-    return item.toJSON();
-  });
-  for (const stu of students) {
-    const td = new Parse.Query("TaskDetail");
-    td.equalTo("task", taskid);
-    td.equalTo("student", stu.objectId);
-    const count = await td.count();
-    if (!count) {
-      const TaskDetail = Parse.Object.extend("TaskDetail");
-      const taskDetail = new TaskDetail();
-      taskDetail.set("status", false);
-      taskDetail.set("isDelete", false);
-      taskDetail.set("company", {
-        __type: "Pointer",
-        className: "Company",
-        objectId: companyid,
-      });
-      taskDetail.set("task", {
-        __type: "Pointer",
-        className: "Task",
-        objectId: taskid,
-      });
-      taskDetail.set("class", {
-        __type: "Pointer",
-        className: "Class",
-        objectId: classid,
-      });
-      taskDetail.set("student", {
-        __type: "Pointer",
-        className: "Student",
-        objectId: stu.objectId,
-      });
-      await taskDetail.save();
-    }
-  }
-});
+const task = require('./Cloud/Task')
+const modules = {
+  beforeSave: [],
+  afterSave: [],
+  beforeDelete: [],
+  afterDelete: [],
+  ...task
+}
+Object.keys(modules).forEach(key => {
+  modules[key].forEach(item => {
+    Parse.Cloud[key](item.class, item.func)
+  })
+})
